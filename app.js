5ab07dfd8033a7358d2ba1fd36d4e3f26561b4eb
merve <iamstreetspirit@gmail.com>
2020-10-10 19:39:10 +0300

    commit

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var personRouter = require('./routes/person');
 var carRouter = require('./routes/car');
 
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/person', personRouter);
 app.use('/car', carRouter);
 
